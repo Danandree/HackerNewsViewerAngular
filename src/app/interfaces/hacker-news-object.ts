c6a041616ec5d2cbcf981adee6ae16bc4819d033
Danandree <danieleandree91@gmail.com>
2024-03-09 17:09:13 +0100

    commit

--- src/app/interfaces/hacker-news-object.ts
+++ src/app/interfaces/hacker-news-object.ts
@@ -1,30 +1,26 @@
 export interface HackerNewsObject {
-    // [key: string]: any;
     id: number;
+    by: string;
+    descendants: number;
+    kids?: number[];
+    score: number;
+    text?: string;
+    type: string;
     title: string;
     time: number;
     url?: string;
-    // by: string;
-    // descendants: number;
-    // kids?: number[];
-    // score: number;
-    // text?: string;
-    // type: string;
 }
 
 export class NEWS implements HackerNewsObject {
-    // [key: string]: any;
     id = 0;
     title = '';
     time = 0;
+    by = '';
+    descendants = 0;
+    score = 0;
+    type = '';
     url?: string;
     date?: string;
     domain?: string;
-    // by = '';
-    // descendants = 0;
-    // kids: number[] = [];
-    // score = 0;
-    // text = '';
-    // type = '';
 
 }
